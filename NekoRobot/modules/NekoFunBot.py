00d24aad29a69066d05f11c7c9cf6cde30c07a34
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-06 21:20:16 +0530

    Update NekoFunBot.py

--- NekoRobot/modules/NekoFunBot.py
+++ NekoRobot/modules/NekoFunBot.py
@@ -120,14 +120,14 @@ def sleep(update, context):
         msg.reply_video(sleepme)
 
 
-KISS_HANDLER = MessageHandler(Filters.regex("(?i)Neko kiss"), kiss, run_async=True)
-BLUSH_HANDLER = MessageHandler(Filters.regex("(?i)Neko blush"), blush, run_async=True)
-PAT_HANDLER = MessageHandler(Filters.regex("(?i)Neko pat"), pat, run_async=True)
-HUG_HANDLER = MessageHandler(Filters.regex("(?i)Neko hug"), hug, run_async=True)
-SLAP_HANDLER = MessageHandler(Filters.regex("(?i)Neko slap"), slap, run_async=True)
-CUTE_HANDLER = MessageHandler(Filters.regex("(?i)Neko cute"), cute, run_async=True)
+KISS_HANDLER = MessageHandler(Filters.regex("(?i)Chizuru kiss"), kiss, run_async=True)
+BLUSH_HANDLER = MessageHandler(Filters.regex("(?i)Chizuru blush"), blush, run_async=True)
+PAT_HANDLER = MessageHandler(Filters.regex("(?i)Chizuru pat"), pat, run_async=True)
+HUG_HANDLER = MessageHandler(Filters.regex("(?i)Chizuru hug"), hug, run_async=True)
+SLAP_HANDLER = MessageHandler(Filters.regex("(?i)Chizuru slap"), slap, run_async=True)
+CUTE_HANDLER = MessageHandler(Filters.regex("(?i)Chizuru cute"), cute, run_async=True)
 SLEEP_HANDLER = MessageHandler(
-    Filters.regex("(Neko sleep|sleep)"), sleep, run_async=True
+    Filters.regex("(Chizuru sleep|sleep)"), sleep, run_async=True
 )
 
 NEKO_PTB.add_handler(KISS_HANDLER)
