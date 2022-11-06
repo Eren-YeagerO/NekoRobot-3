48f7c8bbc1309911dd34127a89f5e53c4214b535
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-07 04:35:36 +0530

    Update mention_all.py

--- NekoRobot/modules/mention_all.py
+++ NekoRobot/modules/mention_all.py
@@ -1,34 +1,3 @@
-"""
-BSD 2-Clause License
-
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
-Copyright (c) 2022-2023, Programmer • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import asyncio
 
 from telethon import events
@@ -36,24 +5,23 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from NekoRobot import tbot
+from NekoRobot import telethn as client
 
 spam_chats = []
 
 
-@tbot.on(
-    events.NewMessage(
-        pattern="^/tagall|/call|/tall|/all|/mentionall|#all|@all|@mentionall|@tagall|@utag(.*)",
-    )
-)
-async def all(event):
+@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
+@client.on(events.NewMessage(pattern="^@all ?(.*)"))
+async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
-        return await event.respond("This command Can Be Use In Groups And Channels/n")
+        return await event.respond(
+            "__This command can be use in groups and channels!__"
+        )
 
     is_admin = False
     try:
-        partici_ = await tbot(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
     except UserNotParticipantError:
         is_admin = False
     else:
@@ -62,40 +30,40 @@ async def all(event):
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("Only admins can mention all!")
+        return await event.respond("__Only admins can mention all!__")
 
     if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("Give me one argument!")
-    if event.pattern_match.group(1):
+        return await event.respond("__Give me one argument!__")
+    elif event.pattern_match.group(1):
         mode = "text_on_cmd"
         msg = event.pattern_match.group(1)
     elif event.is_reply:
         mode = "text_on_reply"
         msg = await event.get_reply_message()
-        if msg is None:
+        if msg == None:
             return await event.respond(
-                "I Can't Mention Members For Older Messages! (messages which are sent before I'm added to group)"
+                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__"
             )
     else:
         return await event.respond(
-            "Reply To a Message Or Give Me Some Text To Mention Others"
+            "__Reply to a message or give me some text to mention others!__"
         )
 
     spam_chats.append(chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in tbot.iter_participants(chat_id):
-        if chat_id not in spam_chats:
+    async for usr in client.iter_participants(chat_id):
+        if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-        if usrnum == 10:
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        if usrnum == 5:
             if mode == "text_on_cmd":
-                txt = f"{usrtxt}\n\n{msg}\n\nMeet Me Here🙈 @Besties_XD ✨🥀"
-                await tbot.send_message(chat_id, txt)
+                txt = f"{msg}\n{usrtxt}"
+                await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
             usrnum = 0
             usrtxt = ""
     try:
@@ -104,15 +72,34 @@ async def all(event):
         pass
 
 
-@tbot.on(events.NewMessage(pattern="^/cancel$"))
+@client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
-    if event.chat_id not in spam_chats:
-        return await event.respond("There Is No Proccess On Going")
+    if not event.chat_id in spam_chats:
+        return await event.respond("__There is no proccess on going...__")
+    is_admin = False
     try:
-        spam_chats.remove(event.chat_id)
-    except:
-        pass
-    return await event.respond("Mentioning Are Stopped")
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if isinstance(
+            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.respond("__Only admins can execute this command!__")
+
+    else:
+        try:
+            spam_chats.remove(event.chat_id)
+        except:
+            pass
+        return await event.respond("__Stopped Mention.__")
 
 
-__mod_name__ = "Mention All"
+__mod_name__ = "Tag All​"
+__help__ = """
+──「 Only for Admins 」──
+
+❍ /tagall or @all '(reply to message or add another message) To mention all members in your group, without exception.'
+"""
