c1d314f1074cd686a0f3731a32bae3fef522cb2d
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-07 05:11:14 +0530

    Update mention_all.py

--- NekoRobot/modules/mention_all.py
+++ NekoRobot/modules/mention_all.py
@@ -5,7 +5,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from NekoRobot import telethn as client
+from NekoRobot import tbot as client
 
 spam_chats = []
 
