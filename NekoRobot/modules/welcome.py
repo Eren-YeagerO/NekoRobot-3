f3fb4cbaf99ce0b4b74099309bd00976ee60d147
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-07 14:37:17 +0530

    Update welcome.py

--- NekoRobot/modules/welcome.py
+++ NekoRobot/modules/welcome.py
@@ -67,10 +67,10 @@ ENUM_FUNC_MAP = {
 
 VERIFIED_USER_WAITLIST = {}
 
-NEKO_IMG = "https://telegra.ph/file/e321b823276a3c54c1dce.mp4"
-NEKO = "https://telegra.ph/file/be5388d1c407d5fe0ca14.jpg"
-NEKO_VID = "https://telegra.ph/file/cf5a6cad607d0172809cf.mp4"
-WAIFUS_IMG = "https://telegra.ph/file/e9284f19f009784e24043.jpg"
+NEKO_IMG = "https://telegra.ph/file/2f07edb1ff6d01bc079f6.mp4"
+NEKO = "https://telegra.ph/file/677cd21f71a3c98edac98.mp4"
+NEKO_VID = "https://telegra.ph/file/4c0dee5f3f55eb5dc312d.mp4"
+WAIFUS_IMG = "https://telegra.ph/file/9cb0905da0c871eba2e1e.mp4"
 
 
 # do not async
