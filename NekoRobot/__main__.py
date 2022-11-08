0653cd7a7078199243301ad4792d320e93be5be8
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-08 14:46:44 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -109,7 +109,6 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 HELP_MSG = "Click The Button Below To Get Help Menu In Your Dm."
-
 START_MSG = "I'm Awake Already!\n<b>Haven't Slept Since:</b> <code>{}</code>"
 
 
