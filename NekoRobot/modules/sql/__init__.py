27a5d26d92b3bf84ada90d39feeb42bb07f2dd0f
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-09 19:28:01 +0530

    Update __init__.py

--- NekoRobot/modules/sql/__init__.py
+++ NekoRobot/modules/sql/__init__.py
@@ -27,11 +27,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from NekoRobot import DB_URI
+from Himawari import DB_URL
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine("postgres://enwfyszs:C4g39JW8GV0vAW9meLAQPwIzDfUS42GO@babar.db.elephantsql.com/enwfyszs")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
