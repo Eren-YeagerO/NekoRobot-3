8227ec879322912cb51aea26d962e2d9fd7d6750
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-11 13:10:15 +0530

    Update image_reverse_search.py

--- NekoRobot/modules/image_reverse_search.py
+++ NekoRobot/modules/image_reverse_search.py
@@ -1,60 +1,42 @@
-"""
-BSD 2-Clause License
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince]
-Copyright (c) 2022-2023, Programmer Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 
 import os
 import re
 import urllib
 import urllib.parse
 import urllib.request
-from urllib.error import HTTPError, URLError
+from urllib.error import URLError, HTTPError
 
 import requests
 from bs4 import BeautifulSoup
-from telegram import InputMediaPhoto, TelegramError, Update
-from telegram.ext import CallbackContext
+from telegram import InputMediaPhoto, TelegramError
 
-from NekoRobot import NEKO_PTB
+from NekoRobot import dispatcher
 from NekoRobot.modules.disable import DisableAbleCommandHandler
+from NekoRobot.modules.helper_funcs.alternate import typing_action
 
 opener = urllib.request.build_opener()
-useragent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
+useragent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.38 "
+    "Safari/537.36 "
+)
+# useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko)
+# Chrome/52.0.2743.98 Mobile Safari/537.36'
 opener.addheaders = [("User-agent", useragent)]
 
 
-def reverse(update: Update, context: CallbackContext):
+@typing_action
+def reverse(update, context):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
 
     msg = update.effective_message
     chat_id = update.effective_chat.id
-    bot, args = context.bot, context.args
     rtmid = msg.message_id
+    args = context.args
     imagename = "okgoogle.png"
 
-    if reply := msg.reply_to_message:
+    reply = msg.reply_to_message
+    if reply:
         if reply.sticker:
             file_id = reply.sticker.file_id
         elif reply.photo:
@@ -64,13 +46,13 @@ def reverse(update: Update, context: CallbackContext):
         else:
             msg.reply_text("Reply to an image or sticker to lookup.")
             return
-        image_file = bot.get_file(file_id)
+        image_file = context.bot.get_file(file_id)
         image_file.download(imagename)
         if args:
             txt = args[0]
             try:
                 lim = int(txt)
-            except:
+            except BaseException:
                 lim = 2
         else:
             lim = 2
@@ -80,7 +62,7 @@ def reverse(update: Update, context: CallbackContext):
             img_link = splatargs[1]
             try:
                 lim = int(splatargs[2])
-            except:
+            except BaseException:
                 lim = 2
         elif len(splatargs) == 2:
             img_link = splatargs[1]
@@ -91,13 +73,14 @@ def reverse(update: Update, context: CallbackContext):
         try:
             urllib.request.urlretrieve(img_link, imagename)
         except HTTPError as HE:
-            if HE.reason == "Not Found":
-                msg.reply_text("Image not found.")
-                return
             if HE.reason == "Forbidden":
                 msg.reply_text(
-                    "Couldn't access the provided link, The website might have blocked accessing to the website by bot or the website does not existed."
+                    "Couldn't access the provided link, The website might have blocked accessing to the website by "
+                    "bot or the website does not existed. "
                 )
+                return
+            if HE.reason == "Not Found":
+                msg.reply_text("Image not found.")
                 return
         except URLError as UE:
             msg.reply_text(f"{UE.reason}")
@@ -107,7 +90,8 @@ def reverse(update: Update, context: CallbackContext):
             return
     else:
         msg.reply_markdown(
-            "Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a link too? `/reverse [picturelink] <amount>`."
+            "Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a "
+            "link too? `/reverse [picturelink] <amount>`. "
         )
         return
 
@@ -121,20 +105,20 @@ def reverse(update: Update, context: CallbackContext):
         fetchUrl = response.headers["Location"]
 
         if response != 400:
-            xx = bot.send_message(
+            xx = context.bot.send_message(
                 chat_id,
                 "Image was successfully uploaded to Google."
-                "\nParsing it, please wait.",
+                "\nParsing source now. Maybe.",
                 reply_to_message_id=rtmid,
             )
         else:
-            xx = bot.send_message(
+            xx = context.bot.send_message(
                 chat_id, "Google told me to go away.", reply_to_message_id=rtmid
             )
             return
 
         os.remove(imagename)
-        match = ParseSauce(f"{fetchUrl}&hl=en")
+        match = ParseSauce(fetchUrl + "&hl=en")
         guess = match["best_guess"]
         if match["override"] and match["override"] != "":
             imgspage = match["override"]
@@ -143,7 +127,7 @@ def reverse(update: Update, context: CallbackContext):
 
         if guess and imgspage:
             xx.edit_text(
-                f"[{guess}]({fetchUrl})\nProcessing...",
+                f"[{guess}]({fetchUrl})\nLooking for images...",
                 parse_mode="Markdown",
                 disable_web_page_preview=True,
             )
@@ -154,8 +138,7 @@ def reverse(update: Update, context: CallbackContext):
         images = scam(imgspage, lim)
         if len(images) == 0:
             xx.edit_text(
-                f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})"
-                "\nCouldn't fetch any images.",
+                f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})",
                 parse_mode="Markdown",
                 disable_web_page_preview=True,
             )
@@ -166,277 +149,11 @@ def reverse(update: Update, context: CallbackContext):
             lmao = InputMediaPhoto(media=str(link))
             imglinks.append(lmao)
 
-        bot.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
+        context.bot.send_media_group(
+            chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid
+        )
         xx.edit_text(
-            f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})",
-            parse_mode="Markdown",
-            disable_web_page_preview=True,
-        )
-    except TelegramError as e:
-        print(e)
-    except Exception as exception:
-        print(exception)
-
-
-def grs(update: Update, context: CallbackContext):
-    if os.path.isfile("okgoogle.png"):
-        os.remove("okgoogle.png")
-
-    msg = update.effective_message
-    chat_id = update.effective_chat.id
-    bot, args = context.bot, context.args
-    rtmid = msg.message_id
-    imagename = "okgoogle.png"
-
-    if reply := msg.reply_to_message:
-        if reply.sticker:
-            file_id = reply.sticker.file_id
-        elif reply.photo:
-            file_id = reply.photo[-1].file_id
-        elif reply.document:
-            file_id = reply.document.file_id
-        else:
-            msg.reply_text("Reply to an image or sticker to lookup.")
-            return
-        image_file = bot.get_file(file_id)
-        image_file.download(imagename)
-        if args:
-            txt = args[0]
-            try:
-                lim = int(txt)
-            except:
-                lim = 2
-        else:
-            lim = 2
-    elif args:
-        splatargs = msg.text.split(" ")
-        if len(splatargs) == 3:
-            img_link = splatargs[1]
-            try:
-                lim = int(splatargs[2])
-            except:
-                lim = 2
-        elif len(splatargs) == 2:
-            img_link = splatargs[1]
-            lim = 2
-        else:
-            msg.reply_text("/reverse <link> <amount of images to return.>")
-            return
-        try:
-            urllib.request.urlretrieve(img_link, imagename)
-        except HTTPError as HE:
-            if HE.reason == "Not Found":
-                msg.reply_text("Image not found.")
-                return
-            if HE.reason == "Forbidden":
-                msg.reply_text(
-                    "Couldn't access the provided link, The website might have blocked accessing to the website by bot or the website does not existed."
-                )
-                return
-        except URLError as UE:
-            msg.reply_text(f"{UE.reason}")
-            return
-        except ValueError as VE:
-            msg.reply_text(f"{VE}\nPlease try again using http or https protocol.")
-            return
-    else:
-        msg.reply_markdown(
-            "Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a link too? `/reverse [picturelink] <amount>`."
-        )
-        return
-
-    try:
-        searchUrl = "https://www.google.com/searchbyimage/upload"
-        multipart = {
-            "encoded_image": (imagename, open(imagename, "rb")),
-            "image_content": "",
-        }
-        response = requests.post(searchUrl, files=multipart, allow_redirects=False)
-        fetchUrl = response.headers["Location"]
-
-        if response != 400:
-            xx = bot.send_message(
-                chat_id,
-                "Image was successfully uploaded to Google."
-                "\nParsing it, please wait.",
-                reply_to_message_id=rtmid,
-            )
-        else:
-            xx = bot.send_message(
-                chat_id, "Google told me to go away.", reply_to_message_id=rtmid
-            )
-            return
-
-        os.remove(imagename)
-        match = ParseSauce(f"{fetchUrl}&hl=en")
-        guess = match["best_guess"]
-        if match["override"] and match["override"] != "":
-            imgspage = match["override"]
-        else:
-            imgspage = match["similar_images"]
-
-        if guess and imgspage:
-            xx.edit_text(
-                f"[{guess}]({fetchUrl})\nProcessing...",
-                parse_mode="Markdown",
-                disable_web_page_preview=True,
-            )
-        else:
-            xx.edit_text("Couldn't find anything.")
-            return
-
-        images = scam(imgspage, lim)
-        if len(images) == 0:
-            xx.edit_text(
-                f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})"
-                "\nCouldn't fetch any images.",
-                parse_mode="Markdown",
-                disable_web_page_preview=True,
-            )
-            return
-
-        imglinks = []
-        for link in images:
-            lmao = InputMediaPhoto(media=str(link))
-            imglinks.append(lmao)
-
-        bot.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
-        xx.edit_text(
-            f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})",
-            parse_mode="Markdown",
-            disable_web_page_preview=True,
-        )
-    except TelegramError as e:
-        print(e)
-    except Exception as exception:
-        print(exception)
-
-
-def gg(update: Update, context: CallbackContext):
-    if os.path.isfile("okgoogle.png"):
-        os.remove("okgoogle.png")
-
-    msg = update.effective_message
-    chat_id = update.effective_chat.id
-    bot, args = context.bot, context.args
-    rtmid = msg.message_id
-    imagename = "okgoogle.png"
-
-    if reply := msg.reply_to_message:
-        if reply.sticker:
-            file_id = reply.sticker.file_id
-        elif reply.photo:
-            file_id = reply.photo[-1].file_id
-        elif reply.document:
-            file_id = reply.document.file_id
-        else:
-            msg.reply_text("Reply to an image or sticker to lookup.")
-            return
-        image_file = bot.get_file(file_id)
-        image_file.download(imagename)
-        if args:
-            txt = args[0]
-            try:
-                lim = int(txt)
-            except:
-                lim = 2
-        else:
-            lim = 2
-    elif args:
-        splatargs = msg.text.split(" ")
-        if len(splatargs) == 3:
-            img_link = splatargs[1]
-            try:
-                lim = int(splatargs[2])
-            except:
-                lim = 2
-        elif len(splatargs) == 2:
-            img_link = splatargs[1]
-            lim = 2
-        else:
-            msg.reply_text("/reverse <link> <amount of images to return.>")
-            return
-        try:
-            urllib.request.urlretrieve(img_link, imagename)
-        except HTTPError as HE:
-            if HE.reason == "Not Found":
-                msg.reply_text("Image not found.")
-                return
-            if HE.reason == "Forbidden":
-                msg.reply_text(
-                    "Couldn't access the provided link, The website might have blocked accessing to the website by bot or the website does not existed."
-                )
-                return
-        except URLError as UE:
-            msg.reply_text(f"{UE.reason}")
-            return
-        except ValueError as VE:
-            msg.reply_text(f"{VE}\nPlease try again using http or https protocol.")
-            return
-    else:
-        msg.reply_markdown(
-            "Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a link too? `/reverse [picturelink] <amount>`."
-        )
-        return
-
-    try:
-        searchUrl = "https://www.google.com/searchbyimage/upload"
-        multipart = {
-            "encoded_image": (imagename, open(imagename, "rb")),
-            "image_content": "",
-        }
-        response = requests.post(searchUrl, files=multipart, allow_redirects=False)
-        fetchUrl = response.headers["Location"]
-
-        if response != 400:
-            xx = bot.send_message(
-                chat_id,
-                "Image was successfully uploaded to Google."
-                "\nParsing it, please wait.",
-                reply_to_message_id=rtmid,
-            )
-        else:
-            xx = bot.send_message(
-                chat_id, "Google told me to go away.", reply_to_message_id=rtmid
-            )
-            return
-
-        os.remove(imagename)
-        match = ParseSauce(f"{fetchUrl}&hl=en")
-        guess = match["best_guess"]
-        if match["override"] and match["override"] != "":
-            imgspage = match["override"]
-        else:
-            imgspage = match["similar_images"]
-
-        if guess and imgspage:
-            xx.edit_text(
-                f"[{guess}]({fetchUrl})\nProcessing...",
-                parse_mode="Markdown",
-                disable_web_page_preview=True,
-            )
-        else:
-            xx.edit_text("Couldn't find anything.")
-            return
-
-        images = scam(imgspage, lim)
-        if len(images) == 0:
-            xx.edit_text(
-                f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})"
-                "\nCouldn't fetch any images.",
-                parse_mode="Markdown",
-                disable_web_page_preview=True,
-            )
-            return
-
-        imglinks = []
-        for link in images:
-            lmao = InputMediaPhoto(media=str(link))
-            imglinks.append(lmao)
-
-        bot.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
-        xx.edit_text(
-            f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})",
+            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})",
             parse_mode="Markdown",
             disable_web_page_preview=True,
         )
@@ -447,8 +164,6 @@ def gg(update: Update, context: CallbackContext):
 
 
 def ParseSauce(googleurl):
-    """Parse/Scrape the HTML code for the info we want."""
-
     source = opener.open(googleurl).read()
     soup = BeautifulSoup(source, "html.parser")
 
@@ -458,7 +173,7 @@ def ParseSauce(googleurl):
         for bess in soup.findAll("a", {"class": "PBorbe"}):
             url = "https://www.google.com" + bess.get("href")
             results["override"] = url
-    except:
+    except BaseException:
         pass
 
     for similar_image in soup.findAll("input", {"class": "gLFyf"}):
@@ -497,26 +212,10 @@ def scam(imgspage, lim):
 
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["reverse", "pp", "PP", "Pp"],
+    ["google", "reverse", "grs", "p", "pp", "name"],
     reverse,
     pass_args=True,
-    admin_ok=True,
     run_async=True,
-)
-GRS_HANDLER = DisableAbleCommandHandler(
-    "grs", grs, pass_args=True, admin_ok=True, run_async=True
-)
-GG_HANDLER = DisableAbleCommandHandler(
-    "gg", gg, pass_args=True, admin_ok=True, run_async=True
 )
 
 NEKO_PTB.add_handler(REVERSE_HANDLER)
-NEKO_PTB.add_handler(GRS_HANDLER)
-NEKO_PTB.add_handler(GG_HANDLER)
-
-__mod_name__ = "Reverse"
-__help__ = """
-*Reverse*
- ❍ `/pp` : Please reply to a sticker, or an image to search it!
- ❍ `/reverse` : Please reply to a sticker, or an image to search it!
-"""
