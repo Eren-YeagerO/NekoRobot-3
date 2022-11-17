5650f761371fc80484df7dba35dba77f6d50ce60
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-18 04:51:42 +0530

    Update wish.py

--- NekoRobot/modules/wish.py
+++ NekoRobot/modules/wish.py
@@ -1,8 +1,27 @@
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2021-2022, Awesome-RJ, [ https://github.com/Awesome-RJ ]
+Copyright (c) 2021-2022, Yūki • Black Knights Union, [ https://github.com/Awesome-RJ/CutiepiiRobot ]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import random
 
-from telethon import events
-
-from NekoRobot import DEV_USERS, OWNER_ID, tbot as neko
+from NekoRobot import DEV_USERS, OWNER_ID, tbot as telethn
 
 from telethon import events, Button
 from telegram import ParseMode
@@ -18,16 +37,7 @@ WISH = """
 /wish I had someone to /cuddle at night...`
 """
 
-GIF = (
-    "https://telegra.ph/file/ef94f2f61aa4d9394ef23.mp4",
-    "https://telegra.ph/file/b82442bf9ebc32534f7a2.mp4",
-    "https://telegra.ph/file/70d43e136125f9c120d2e.mp4",
-    "https://telegra.ph/file/45354d3e42982f8de78f4.mp4",
-    "https://telegra.ph/file/a22a0930f069686a0c4ef.mp4",
-)
-
-
-@neko.on(events.NewMessage(pattern="/wish ?(.*)"))
+@telethn.on(events.NewMessage(pattern="/wish ?(.*)"))
 async def wish(e):
  quew = e.pattern_match.group(1)
  if e.sender_id != DEV_USERS and not quew:
