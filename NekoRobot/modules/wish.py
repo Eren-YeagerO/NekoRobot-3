adb3527092120194b6b81a0f69dd32f2c4b5c673
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-18 12:15:25 +0530

    Update wish.py

--- NekoRobot/modules/wish.py
+++ NekoRobot/modules/wish.py
@@ -19,11 +19,11 @@ async def wish(e):
     if e.is_reply:
         mm = random.randint(1, 100)
         lol = await e.get_reply_message()
-        fire = "https://telegra.ph/file/4afafb6a47c674c5a1563.mp4"
+        fire = "https://telegra.ph/file/cae00f6c0729da2a93315.mp4"
         await neko.send_file(
             e.chat_id,
             fire,
-            caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n__chance of success {mm}%__",
+            caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__chance of success {mm}%__",
             reply_to=lol,
         )
     if not e.is_reply:
@@ -32,6 +32,6 @@ async def wish(e):
         await neko.send_file(
             e.chat_id,
             fire,
-            caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n__chance of success {mm}%__",
+            caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__chance of success {mm}%__",
             reply_to=e,
         )
