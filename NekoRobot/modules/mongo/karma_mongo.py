2a879299c19d0e33910e5eb694ff80051a29d4ee
Eren-YeagerO <107749289+Eren-YeagerO@users.noreply.github.com>
2022-11-20 14:02:11 +0530

    Update karma_mongo.py

--- NekoRobot/modules/mongo/karma_mongo.py
+++ NekoRobot/modules/mongo/karma_mongo.py
@@ -29,7 +29,7 @@ karmadb = db.karma
 karmaonoffdb = db.karmaonoff
 
 
-async def get_karmas_count() -> dict:
+def get_karmas_count() -> dict:
     chats = karmadb.find({"chat_id": {"$lt": 0}})
     if not chats:
         return {}
@@ -44,7 +44,7 @@ async def get_karmas_count() -> dict:
     return {"chats_count": chats_count, "karmas_count": karmas_count}
 
 
-async def user_global_karma(user_id) -> int:
+def user_global_karma(user_id) -> int:
     chats = karmadb.find({"chat_id": {"$lt": 0}})
     if not chats:
         return 0
@@ -58,21 +58,21 @@ async def user_global_karma(user_id) -> int:
     return total_karma
 
 
-async def get_karmas(chat_id: int) -> Dict[str, int]:
+def get_karmas(chat_id: int) -> Dict[str, int]:
     karma = karmadb.find_one({"chat_id": chat_id})
     if not karma:
         return {}
     return karma['karma']
 
 
-async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
+def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
     name = name.lower().strip()
     karmas = get_karmas(chat_id)
     if name in karmas:
         return karmas[name]
 
 
-async def update_karma(chat_id: int, name: str, karma: dict):
+def update_karma(chat_id: int, name: str, karma: dict):
     name = name.lower().strip()
     karmas = get_karmas(chat_id)
     karmas[name] = karma
@@ -81,31 +81,31 @@ async def update_karma(chat_id: int, name: str, karma: dict):
     )
 
 
-async def is_karma_on(chat_id: int) -> bool:
+def is_karma_on(chat_id: int) -> bool:
     chat = karmadb.find_one({"chat_id_toggle": chat_id})
     return not chat
 
 
-async def karma_on(chat_id: int):
+def karma_on(chat_id: int):
     is_karma = is_karma_on(chat_id)
     if is_karma:
         return
     return karmadb.delete_one({"chat_id_toggle": chat_id})
 
 
-async def karma_off(chat_id: int):
+def karma_off(chat_id: int):
     is_karma = is_karma_on(chat_id)
     if not is_karma:
         return
     return karmadb.insert_one({"chat_id_toggle": chat_id})
 
-async def int_to_alpha(user_id: int) -> str:
+def int_to_alpha(user_id: int) -> str:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     user_id = str(user_id)
     return "".join(alphabet[int(i)] for i in user_id)
 
 
-async def alpha_to_int(user_id_alphabet: str) -> int:
+def alpha_to_int(user_id_alphabet: str) -> int:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     user_id = ""
     for i in user_id_alphabet:
